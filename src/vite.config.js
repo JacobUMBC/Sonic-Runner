3968079834237b849b31d7824b5806fbfb387b23
JacobUMBC <jacobr5@umbc.edu>
2024-12-30 20:48:26 -0500

    Deploy sonic

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "./",
-
+  base: "/Sonic-Runner/", // Replace 'Sonic-Runner' with your repository name
 });
